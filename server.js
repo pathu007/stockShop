f0334558b1086c2cd8c0e7cdedc2e31e984fd51c
PARTH KACHHADIA <12bit011@nirmauni.ac.in>
2022-02-03 22:06:58 -0700

    Parth Kachhadia

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req,res) {
+
+	console.log(__dirname);
+    
+res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+// Start the app by listening on the default Heroku port
+console.log('Console is listening');
